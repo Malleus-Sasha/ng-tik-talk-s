63412cc0217f32aa627ea3acf636555f0ffa1ddf
Alex <amksandr@gmail.com>
2024-11-25 17:48:59 +0200

    TTS: Refresh (fake token).

--- src/app/services/auth/auth.interceptor.ts
+++ src/app/services/auth/auth.interceptor.ts
@@ -3,17 +3,24 @@ import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
 import { catchError, switchMap, throwError } from 'rxjs';
 
+let isRefreshing = false;
+
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   console.log(':Intercept:Auth: ', req);
   const authService = inject(AuthService);
   const token = authService.token;
 
   if (!token) return next(req);
+
+  if (isRefreshing) {
+    return refreshAndProceed(authService, req, next);
+  }
   
   return next(addHeaderToken(req, token)).pipe(
     catchError((error) => {
-      console.log(':Http:Error:', error);
-      if (error.status === 403) {
+      console.log(':Http:Error:', error.status);
+      // Unauthorized status 401
+      if (error.status === 401) {
         return refreshAndProceed(authService, req, next);
       }
       return throwError(error);
@@ -25,11 +32,17 @@ const refreshAndProceed = (
   authService: AuthService, 
   req: HttpRequest<any>, 
   next: HttpHandlerFn ) => {
-  return authService.refreshAuthToken().pipe(
-    switchMap((res) => {
-      return next(addHeaderToken(req, res.accessToken));
-    })
-  );
+    if (!isRefreshing) {
+      isRefreshing = true;
+      return authService.refreshAuthToken().pipe(
+        switchMap((res) => {
+          isRefreshing = false;
+          return next(addHeaderToken(req, res.accessToken));
+        })
+      );
+    }
+  
+    return next(addHeaderToken(req, authService.token!));
 }
 
 const addHeaderToken = (req: HttpRequest<any>, token: string) => {
