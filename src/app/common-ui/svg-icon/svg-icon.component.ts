7fa291f1cab32a132493189ea4a993d501a38632
Alex <amksandr@gmail.com>
2024-11-26 19:53:00 +0200

    TTS: Added svg icons component.

---
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'svg[icon]',
+  standalone: true,
+  imports: [],
+  template: '<svg:use [attr.href]="href"></svg:use>',
+  styles: ''
+})
+export class SvgIconComponent {
+  @Input() icon = '';
+
+  get href() {
+    console.log('[svg]', this.icon);
+    return `/assets/icons/${this.icon}.svg#${this.icon}`;
+  }
+}
